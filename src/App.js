6fd05a7c6e293f56d3949261c3c4662a57c01119
vamshigoud9876 <vamshigoud9876.gmail.com>
2022-10-24 08:07:24 +0530

    added1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 function App() {
   const APP_KEY = "21a9882837fcacebe603dc98696d6333";
   const API_Id = "7c49f72a";
+  const inputRef = useRef(null);
   const [ingredientsList, updateIngredientsList] = useState([]);
+  const search = ()=>{
+    searchForRecipe(inputRef.current.value);
+  }
   const searchForRecipe = (query) => {
     const url = `search?q=${query}&app_id=${API_Id}&app_key=${APP_KEY}`;
     fetch(url, {mode: "no-cors"}).then(response => { 
@@ -19,22 +23,26 @@ function App() {
     })
   }
   useEffect(()=>{
-    searchForRecipe("chicken");
-  })
+    searchForRecipe("mutton");
+  },[])
   return (
     <div className='Apps'>
       <header className='App-header'>
+        <div>
+          <input ref={inputRef} placeholder='Search Recipe' />
+          <button onClick={search}>search</button>
+        </div>
         <div className='Wrapper'>
-          <h1>Hello</h1>
           {ingredientsList.map((item)=>{
             return (
               <div className='Ingredient'>
-                <h1>{item}</h1>
                 <span>{item.recipe.label}</span>
-                {/* <img src={item.recipe.image} alt="loading" />
-                {item.recipe.ingredientsLines.map((step)=>{
-                 return <p>{step}</p>;
-                })} */}
+                <img src={item.recipe.image} alt="loading" />
+                <div className='steps'>
+                {item.recipe.ingredientLines.map((step)=>{
+                  return <p>{step}</p>;
+                })}
+                </div>
               </div>
             )
           })}   
